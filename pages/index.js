d46cd019953753416aede134a8ffb3d31c235e7c
Shiv <shivg03@gmail.com>
2022-09-13 18:32:00 +0530

    cleaning up code

--- pages/index.js
+++ pages/index.js
@@ -24,15 +24,6 @@ export default function Home() {
     setShowMe(true);
   }
   const [type, setType] = useState('cover');
-  let BackgroundImage;
-    switch (image) {
-      case 'https://github.com/PatrickJaiin/PatrickJaiin/blob/main/images/dark.jpg?raw=true':
-        BackgroundImage = <div style={{ background: `url(${image})`, backgroundSize: `cover`}} className=" h-screen w-full flex justify-center" ></div>;
-        break;
-      case 'https://cdn.discordapp.com/attachments/941091409509896283/951093872702939196/catalina.jpg':
-        BackgroundImage = <div style={{ background: `url(${image})`, backgroundSize: 'cover'}} className=" h-screen w-full flex justify-center" ></div>;
-        break;
-   }
   return (
     <div style={{ background: `url(${image})`, backgroundSize: `${type}`}} className=" h-screen w-full flex justify-center">
       <div className=" pt-24 flex-col h-[78%] w-[54%]">
