41161b61d72662972fd6632b11c529592e3845f0
Shiv <shivg03@gmail.com>
2025-11-21 15:20:17 -0500

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -8,7 +8,7 @@ export default function Home() {
       <Head>
         <title>ShivOS</title>
         <meta name="description" content="Shiv's macOS Portfolio" />
-        <link rel="icon" href="/favicon.ico" />
+        <link rel="icon" href="data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%220 0 100 100%22><text y=%22.9em%22 font-size=%2290%22>🫡</text></svg>" />
       </Head>
       <Desktop />
     </>
